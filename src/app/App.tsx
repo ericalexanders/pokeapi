67dc227357248650cf4d8d4678c67c76fe0f001f
Eric Alexander <ericalexanders@gmail.com>
2022-08-06 22:53:29 -0300

    First base

---
+++ src/app/App.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { ThemeProvider } from "styled-components";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
+import { GlobalStyle, theme } from "./styles";
+import { Bg } from "./styled-app";
+
+const queryClient = new QueryClient();
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Bg />
+      </ThemeProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
